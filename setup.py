5956a7ef63aaa52065dc0434dde68873d9274b6c
Gabriel Moreira <gmaestre02@gmail.com>
2020-08-18 10:07:00 -0300

    Update setup.py
    Simple Up-to-Date.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 setup(
       name='my_custom_sklearn_transforms',
       version='1.0',
@@ -8,9 +7,9 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
+      url='https://github.com/GabrielMaestre/sklearn_transforms/',
+      author='Vanderlei Munhoz | Gabriel Moreira',
+      author_email='vnderlev@protonmail.ch | gmaestre02@gmail.com',
       license='BSD',
       packages=[
             'my_custom_sklearn_transforms'
